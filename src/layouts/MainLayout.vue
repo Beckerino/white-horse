b63defa30272e40cefa6ab68b0fd441325a766c3
Diego Paiva <paivadiego7797@gmail.com>
2020-02-26 19:19:45 -0400

    [Template] Alteração footer/ menus

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,22 +15,18 @@
     </q-header>
 
     <q-drawer show-if-above v-model="left" side="left" bordered>
-      EAE MEN KK
+      <p class="text-h6">Menus</p>
+      <div v-for="(item, index) in menus" :key="index" class="row q-py-xs">
+        <q-btn class="col self-center" :color="item.color" :to="item.to" flat >{{item.title}}</q-btn>
+      </div>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <q-footer elevated class="bg-grey-8 text-white">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Title
-        </q-toolbar-title>
-      </q-toolbar>
+    <q-footer elevated class="bg-grey-8 text-white text-center">
+      Todos os direitos reservados &copy; {{date}}
     </q-footer>
 
   </q-layout>
@@ -38,9 +34,33 @@
 
 <script>
 export default {
+  computed: {
+    date () {
+      return new Date().getFullYear();
+    }
+  },
   data () {
     return {
-      left: false
+      left: true,
+      usuario: {
+      },
+      menus : [
+        {
+          title: "Eae men kk ",
+          to: "/toroute",
+          color: "red"
+      },
+      {
+          title: "Eae men kk ",
+          to: "/toroute",
+          color: "blue"
+      },
+      {
+          title: "Eae men kk ",
+          to: "/toroute",
+          color: "green"
+      },
+      ]
     }
   }
 }
